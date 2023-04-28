868b57d9ba31ca037ad3deda17cca826364ba90e
Cloverrrrr <87894856+Cloverrrrr@users.noreply.github.com>
2023-04-28 15:04:47 +0800

    Create backbone_selector.py

---
+++ HRPN/model/backbones/backbone_selector.py
@@ -0,0 +1,50 @@
+##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## Created by: Donny You, RainbowSecret
+## Microsoft Research
+## yuyua@microsoft.com
+## Copyright (c) 2019
+##
+## This source code is licensed under the MIT-style license found in the
+## LICENSE file in the root directory of this source tree
+##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import sys
+sys.path.append('/media/aiilab/Experiments/WangShouju/HRNET/yx3/utils')
+# from model.backbones.resnet.resnet_backbone import ResNetBackbone
+from model.backbones.hrnet.hrnet_backbone import HRNetBackbone
+from model.backbones.hrt.hrt_backbone import HRTBackbone
+# from model.backbones.swin.swin_backbone import SwinTransformerBackbone
+from logger import Logger as Log
+
+
+class BackboneSelector(object):
+    def __init__(self, configer):
+        self.configer = configer
+
+    def get_backbone(self, **params):
+        backbone = self.configer.get("network", "backbone")
+
+        model = HRNetBackbone(self.configer)(**params)
+        # if (
+        #     "resnet" in backbone or "resnext" in backbone or "resnest" in backbone
+        # ) and "senet" not in backbone:
+        #     model = ResNetBackbone(self.configer)(**params)
+        #
+        # elif "hrt" in backbone:
+        #     model = HRTBackbone(self.configer)(**params)
+        #
+        # elif "hrnet" in backbone:
+        #     model = HRNetBackbone(self.configer)(**params)
+        #
+        # elif "swin" in backbone:
+        #     model = SwinTransformerBackbone(self.configer)(**params)
+        #
+        # else:
+        #     Log.error("Backbone {} is invalid.".format(backbone))
+        #     exit(1)
+
+        return model
